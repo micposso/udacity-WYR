8d1b4741f17e9e5a57dbbfc3609edb0678e7f349
unknown <micposso@gmail.com>
2019-03-11 23:30:12 -0400

    add redux libraries, add store and clean up the app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import App from './components/App';
+
+function reducer (state='', action) {
+  if (action.type === 'changeState') {
+    return action.payload.newState
+  }
+  return 'State from Reducer is working'
+}
+
+const store = createStore(reducer)
+
+console.log(store.getState());
+
+const action = {
+  type: 'changeState',
+  payload: {
+    newState: 'State from action is working'
+  }
+}
+
+store.dispatch(action)
+
+console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
