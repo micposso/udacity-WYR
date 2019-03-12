03cb92add5ca75da22cda3bf422dfac1428994d3
unknown <micposso@gmail.com>
2019-03-11 23:39:12 -0400

    added css file to app component and created login component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import LogIn from './LogInComponent'
+import '../App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          Redux app
+          <LogIn />
         </header>
       </div>
     );
