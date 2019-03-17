9f68bc9be786439d9ea794c368396dce7120d595
unknown <micposso@gmail.com>
2019-03-17 15:52:45 -0400

    add provider to root file and pass combined reducer, fix import statements in combined reducer

--- src/index.js
+++ src/index.js
@@ -1,30 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from './components/App';
+import reducer from './reducers'
 import 'semantic-ui-css/semantic.min.css'
-
-function reducer (state='', action) {
-  if (action.type === 'changeState') {
-    return action.payload.newState
-  }
-  return 'State from Reducer is working'
-}
 
 const store = createStore(reducer)
 
 console.log(store.getState());
 
-const action = {
-  type: 'changeState',
-  payload: {
-    newState: 'State from action is working'
-  }
-}
-
-store.dispatch(action)
-
-console.log(store.getState());
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+document.getElementById('root'));
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from  'redux'
-import { usersReducer } from './usersReducer'
-import { questionsReducer} from './questionsReducer'
+import usersReducer from './usersReducer'
+import questionsReducer from './questionsReducer'
 
 export default combineReducers({
   usersReducer,
