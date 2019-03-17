e5662c4b6ece21d2800523111aed51afbbc29f52
unknown <micposso@gmail.com>
2019-03-17 19:13:03 -0400

    add thunk library and logger middleware to store

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import { Provider } from 'react-redux'
 import App from './components/App';
 import reducer from './reducers'
 import 'semantic-ui-css/semantic.min.css'
+import middleWare from './middleware'
 
-const store = createStore(reducer)
+const store = createStore(reducer, middleWare)
 
 console.log(store.getState());
 
