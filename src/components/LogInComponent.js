ca6e9cf253827c41f653e45fd18a3a759dbea4f6
unknown <micposso@gmail.com>
2019-03-18 00:09:53 -0400

    update

--- src/components/LogInComponent.js
+++ src/components/LogInComponent.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import { Button, Form, Grid, Header, Segment, Dropdown } from "semantic-ui-react";
-import { GiPerspectiveDiceSixFacesRandom  } from "react-icons/gi";
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Segment,
+  Dropdown
+} from "semantic-ui-react";
+import { GiPerspectiveDiceSixFacesRandom as Icon } from "react-icons/gi";
 
 const styles = {
   widths: {
@@ -22,38 +29,64 @@ const userOptions = [
   }
 ];
 
-const LogIn = () => (
-  <div className='login-form'>
-    <style>{`
-      body > div,
-      body > div > div,
-      body > div > div > div.login-form {
-        height: 100%;
-      }
-    `}</style>
-    <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
-      <Grid.Column style={{ maxWidth: `${styles.widths.mainContainer}` }}>
-        <Form size='large'>
-          <Segment stacked>
-            <div><GiPerspectiveDiceSixFacesRandom size={60} color="orange" /></div>
-            <Header as='h2' color='orange' textAlign='center'>
-              The Questions Game
-            </Header>
-            <Dropdown
-              placeholder='Select Friend'
-              fluid
-              selection
-              options={userOptions}
-              style={{marginBottom: `${styles.spacing.dropDown}%`}}
-            />
-            <Button color='orange' fluid size='large'>
-              Login
-            </Button>
-          </Segment>
-        </Form>
-      </Grid.Column>
-    </Grid>
-  </div>
-);
+class LogIn extends Component {
+  render() {
+    console.log('This is the props: ', this.props);
+    
+    return (
+      <div className="login-form">
+        <style>{`
+          body > div,
+          body > div > div,
+          body > div > div > div.login-form {
+            height: 100%;
+          }
+        `}</style>
+        <Grid
+          textAlign="center"
+          style={{ height: "100%" }}
+          verticalAlign="middle"
+        >
+          <Grid.Column style={{ maxWidth: `${styles.widths.mainContainer}` }}>
+            <Form size="large">
+              <Segment stacked>
+                <div>
+                  <Icon size={60} color="orange" />
+                </div>
+                <Header as="h2" color="orange" textAlign="center">
+                  The Questions Game
+                </Header>
+                <Dropdown
+                  placeholder="Select Friend"
+                  fluid
+                  selection
+                  options={userOptions}
+                  style={{ marginBottom: `${styles.spacing.dropDown}%` }}
+                />
+                <Button color="orange" fluid size="large">
+                  Login
+                </Button>
+              </Segment>
+            </Form>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
+}
 
-export default LogIn;
+//get user, userID, name and avartURL and push it into an array that you will pass to the Dropdown component as an options value.
+
+//problem the property users of the state it not getting through the mapStateToProps function
+function mapStateToProps({ users }) {
+  console.log('This is the data from the store: ', users);
+  
+  return {
+    key: '',
+    text: '',
+    value: '',
+    image: ''
+  };
+}
+
+export default connect(mapStateToProps)(LogIn)
