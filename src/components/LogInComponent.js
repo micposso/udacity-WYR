40462a0ffdfd483142a0fc3c16a907f1ada780d5
unknown <micposso@gmail.com>
2019-03-27 00:08:56 -0400

    pass data to props using object.values

--- src/components/LogInComponent.js
+++ src/components/LogInComponent.js
@@ -1,45 +1,39 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { GiPerspectiveDiceSixFacesRandom as Icon } from "react-icons/gi";
-import * as $ from 'jquery';
-import{ findDOMNode } from 'react-dom'
 
 class LogIn extends Component {
   render() {
+    console.log(this.props.avatar);
+     
     return (
       <form className="ui large form">
         <div className="ui stacked segment">
           <div>
             <Icon size={60} color="orange" />
-          <h2>The Questions Game</h2>
+            <h2>The Questions Game</h2>
           </div>
-          <div className="ui fluid selection dropdown" onClick={this.handleDropdown}>
-          <input type="hidden" name="user" />
-          <i className="dropdown icon"></i>
-          <div className="default text">Select Friend</div>
-          <div ref="dropdown"  className="menu">
-            <div className="item" data-value="jenny">
-            <img className="ui mini avatar image" src="/images/avatar/small/jenny.jpg" />
-            Jenny Hess
-            </div>
-            <div className="item" data-value="elliot">
-            <img className="ui mini avatar image" src="/images/avatar/small/elliot.jpg" />
-            Elliot Fu
-          </div>
-          </div>
-          </div>
-          <button className="ui fluid large orange submit button" color="orange">Submit</button>
+          <select>
+            {this.props.avatar.map((user) => (
+                <option key={user.id} value={user.id}>{user.id}</option>
+            ))}
+          </select>
+          <button
+            className="ui fluid large orange submit button"
+            color="orange"
+          >
+            Submit
+          </button>
         </div>
       </form>
     );
   }
 }
 
-//get user, userID, name and avartURL and push it into an array that you will pass to the Dropdown component as an options value.
-
-//problem the property users of the state it not getting through the mapStateToProps function
 function mapStateToProps({ usersReducer }) {
-  return {};
+  return {
+    avatar: Object.values(usersReducer)
+  };
 }
 
 export default connect(mapStateToProps)(LogIn);
