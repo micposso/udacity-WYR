f27fcbee0b4dcdeeda106fa6c151067486c88c2c
unknown <micposso@gmail.com>
2019-04-08 00:15:02 -0400

    add react router

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Grid, Container } from "semantic-ui-react";
 import { handleInitialData } from "../actions/shared";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import "../css/app.css";
 import LogIn from "./LogInComponent";
+import LeaderBoard from "./LeaderBoardComponent";
 
 class App extends Component {
   componentDidMount() {
@@ -14,7 +16,12 @@ class App extends Component {
       <Container textAlign="center" verticalalign="middle">
         <Grid>
           <Grid columns={4}>
-            <LogIn />
+          <Router>
+          <Switch>
+            <Route path='/' exact component={LogIn} />
+            <Route path='/leaderBoard' exact component={LeaderBoard} />
+          </Switch>
+          </Router>
           </Grid>
         </Grid>
       </Container>
